3c9907b358c6d781d16e6a741f2afd7902ded20d
40623 <40623@SRV-18>
2026-09-23 11:54:10 +0300

    выполнение практической

---
+++ TodoListApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TodoListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Работу сделали Погиба и Гуляев!");
+
+            Console.WriteLine("Введите своё имя: ");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine("Введите свою фамилию: ");
+            var lastName = Console.ReadLine();
+
+            Console.WriteLine("Введите год рождения: ");
+            string birthDate = Console.ReadLine();
+
+            int years = int.Parse(birthDate);
+            years = 2026 - years;
+
+            Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, год - {years}.");
+        }
+    }
+}
